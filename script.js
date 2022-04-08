726d77305d5891ced3e4bfd7f9ba68173af369c2
Herman <hermnchn@gmail.com>
2022-04-07 23:21:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,17 @@ const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate
 
 //Get Jokes from Joke API
 async function getJokes() {
+    let joke = '';
     const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        console.log(joke);
     } catch (error) {
         // Catch Errors Here
         console.log('whoops', error);
